9f52408c7ebdc4a9d0a9245e35ad676f77a78c3b
mikkycody <my@personal>
2023-02-05 21:05:30 +0100

    feat: get all unpaid jobs

--- src/controllers/job/jobController.js
+++ src/controllers/job/jobController.js
@@ -17,7 +17,10 @@ import HttpStatus from "http-status";
  */
 const createJob = async (req, res, next) => {
   try {
-    const { error, value: payload } = await validateCreateJob({...req.body, userId: req.user.id});
+    const { error, value: payload } = await validateCreateJob({
+      ...req.body,
+      userId: req.user.id,
+    });
     if (error) {
       return response(res, error.message, HttpStatus.BAD_REQUEST);
     }
@@ -28,4 +31,23 @@ const createJob = async (req, res, next) => {
   }
 };
 
-export { createJob };
+/**
+ * @function
+ * @async
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {function} next - Express next middleware function
+ * @returns {Promise<Object>} Returns a response object with all unpaid jobs
+ *
+ * Finds all unpaid jobs with active contract.
+ */
+const unpaidJobs = async (req, res, next) => {
+  try {
+    const jobs = await jobService.unpaidJobs(req.user.id);
+    return response(res, "Jobs Retrieved", HttpStatus.OK, jobs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createJob, unpaidJobs };
--- src/services/job/jobService.js
+++ src/services/job/jobService.js
@@ -1,7 +1,54 @@
-import { Job } from "../../models";
+import { Job, Contract, Profile } from "../../models";
+import { ContractStatusEnum } from "../../enums";
+import Sequelize from "sequelize";
 
+const { Op } = Sequelize;
+/**
+ * Creates a new job record.
+ * @async
+ * @function
+ * @param {Object} payload - The job data to be stored.
+ * @returns {Promise<Object>} - A promise that resolves to the newly created job record.
+ */
 const create = async (payload) => {
   return Job.create(payload);
 };
 
-export default { create };
+/**
+ * Retrieves all unpaid jobs.
+ * @async
+ * @function
+ * @param {String} userId - The ID of the user to retrieve unpaid jobs for.
+ * @returns
+ */
+const unpaidJobs = async (userId) => {
+  return Job.findAll({
+    where: {
+      isPaid: false,
+    },
+    include: [
+      {
+        model: Contract,
+        as: "contract",
+        include: [
+          {
+            model: Profile,
+            as: "client",
+            attributes: { exclude: ["balance"] },
+          },
+          {
+            model: Profile,
+            as: "contractor",
+            attributes: { exclude: ["balance"] },
+          },
+        ],
+        where: {
+          [Op.or]: [{ clientId: userId }, { contractorId: userId }],
+          status: ContractStatusEnum.IN_PROGRESS,
+        },
+      },
+    ],
+  });
+};
+
+export default { create, unpaidJobs };
