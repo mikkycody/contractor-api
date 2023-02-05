62703335bb1430f9b00a7a50e143dff39adbc6f0
mikkycody <my@personal>
2023-02-05 21:31:11 +0100

    feat: job payment

--- src/routes/job/index.js
+++ src/routes/job/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createJob, unpaidJobs } from "../../controllers/job/jobController";
+import { createJob, unpaidJobs, pay } from "../../controllers/job/jobController";
 import getProfile from "../../middlewares/getProfile";
 
 const router = express.Router();
 router.post("/api/v1/jobs", getProfile, createJob);
 router.get("/api/v1/jobs/unpaid", getProfile, unpaidJobs);
+router.post("/api/v1/jobs/:jobId/pay", getProfile, pay);
 
 export default router;
